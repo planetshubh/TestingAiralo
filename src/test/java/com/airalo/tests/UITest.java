df9b9bbd21a8d8a06de34c2d92f83118b5dcd824
Shubh <shubh_er@hotmail.com>
2024-07-23 16:25:09 +0200

    Removed a duplicate method

--- src/test/java/com/airalo/tests/UITest.java
+++ src/test/java/com/airalo/tests/UITest.java
@@ -1,8 +1,6 @@
 package com.airalo.tests;
 
 import com.airalo.pages.HomePage;
-import com.codeborne.selenide.WebDriverRunner;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 
@@ -17,10 +15,5 @@ public class UITest extends BaseTest {
                 .searchForJapan()
                 .selectFirstPackage()
                 .verifyPackageDetails();
-    }
-
-    @AfterMethod
-    public void teardown() {
-        WebDriverRunner.getWebDriver().quit();
     }
 }
